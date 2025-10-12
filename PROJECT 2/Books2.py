1d811d2da32f7286c8e685021e1ea29de61fce22
Muhammad Usman Malik <usmaan.maalik9503@gmail.com>
2025-10-12 22:27:28 +0200

    working further on data validation

--- PROJECT 2/Books2.py
+++ PROJECT 2/Books2.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 #pydantic comes pre-installed with our fastapi installation.
 
@@ -27,17 +28,29 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    book_id: int
-    title: str
-    author: str
-    description: str
-    rating: float
+    #book_id: Optional[int] = None
+    book_id: Optional[int] = Field(description='ID is not needed on create', default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=3)
+    description: str = Field(min_length=1, max_length=100)
+    rating: float = Field(gt=0, lt=6)     #gt means greater than and lt means less than
+
+    model_config = {
+        "json_schema_extra": {             #this "json_schema_extra" and "example" should be same
+            "example": {
+                "title": "A book title",
+                "author": "Book's author name",
+                "description": "Some detail about book",
+                "rating": 5
+            }
+        }
+    }
 
 BOOKS = [
     Book(1, 'Computer Science', 'Harry Brooks', 'Absolutely worth it!', 5),
 Book(2, 'Crazy Techniques', 'Joe Root', 'Should read it once.', 4),
 Book(3, 'A Tall Fast Bowler', 'Stephen Finn', 'Great book', 3.5),
-Book(4, 'Computer Science', 'Harry Brooks', 'Absolutely worth it!', 5),
+Book(4, 'Computer Science', 'James Anderson', 'Absolutely worth it!', 5),
 Book(5, 'Computer Science', 'Shawn Tait', 'Must read', 1),
 Book(600, 'Swimmers', 'Michael Phelps', 'Amazing', 2)
 ]
@@ -59,4 +72,62 @@ async def create_book(book_request: BookRequest):     #book request type of book
     #BOOKS.append(book_request)
     new_book = Book(**book_request.model_dump())    #converting the request to Book object
     print(type(new_book))
-    BOOKS.append(new_book)
+    #BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+'''def find_book_id(book_param: Book):
+    if len(BOOKS) > 0:
+        book_param.id = BOOKS[-1].id + 1
+    else:
+        book_param.id = 1
+    return book_param'''
+
+
+#we can also write the above function with ternary operator
+def find_book_id(book_param: Book):
+    book_param.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book_param
+
+
+@book_app.get('/books/{book_id}')
+async def create_book(book_id: int):
+    for b in BOOKS:
+        if b.id == book_id:
+            return b
+
+
+'''@book_app.get('/books/{book_rating}')           
+async def find_book_by_rating(book_rating: float):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return'''
+
+#So we cannot find out books by rating through path parameter concept for this we need to use query parameter,
+#that's why I made the function again with query parameter.Query parameter is for filter data, path parameter is use to
+#take out specific information (e.g., here it take out only one object)
+
+@book_app.get('/books_rating')
+async def find_book_by_rating(book_rating: float):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@book_app.put('/books/update_book')
+async def update_book(book_req: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_req.book_id:
+            BOOKS[i] = book_req
+
+
+@book_app.delete('/delete_books/{b_id}')
+async def delete_books(b_id: int):
+    for b in range(len(BOOKS)):
+        if BOOKS[b].id == b_id:
+            BOOKS.pop(b)
+            break
