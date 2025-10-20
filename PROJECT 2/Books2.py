3c53fa9dac9a81248fd91f89ec77160459dc1ddb
Muhammad Usman Malik <usmaan.maalik9503@gmail.com>
2025-10-20 23:20:37 +0200

    working on validation in query parameter and path parameter

--- PROJECT 2/Books2.py
+++ PROJECT 2/Books2.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 #pydantic comes pre-installed with our fastapi installation.
 
@@ -19,13 +20,15 @@ class Book:
     author: str
     description: str
     rating: float
+    published_date: int
 
-    def __init__(self, book_id, title, author, description, rating):
+    def __init__(self, book_id, title, author, description, rating, published_date):
         self.id = book_id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 class BookRequest(BaseModel):
     #book_id: Optional[int] = None
@@ -34,6 +37,8 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=3)
     description: str = Field(min_length=1, max_length=100)
     rating: float = Field(gt=0, lt=6)     #gt means greater than and lt means less than
+    published_date: int = Field(gt=1999, lt=2031)
+
 
     model_config = {
         "json_schema_extra": {             #this "json_schema_extra" and "example" should be same
@@ -41,21 +46,22 @@ class BookRequest(BaseModel):
                 "title": "A book title",
                 "author": "Book's author name",
                 "description": "Some detail about book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2029,
             }
         }
     }
 
 BOOKS = [
-    Book(1, 'Computer Science', 'Harry Brooks', 'Absolutely worth it!', 5),
-Book(2, 'Crazy Techniques', 'Joe Root', 'Should read it once.', 4),
-Book(3, 'A Tall Fast Bowler', 'Stephen Finn', 'Great book', 3.5),
-Book(4, 'Computer Science', 'James Anderson', 'Absolutely worth it!', 5),
-Book(5, 'Computer Science', 'Shawn Tait', 'Must read', 1),
-Book(600, 'Swimmers', 'Michael Phelps', 'Amazing', 2)
+    Book(1, 'Computer Science', 'Harry Brooks', 'Absolutely worth it!', 5, 2030),
+Book(2, 'Crazy Techniques', 'Joe Root', 'Should read it once.', 4, 2030),
+Book(3, 'A Tall Fast Bowler', 'Stephen Finn', 'Great book', 3.5, 2029),
+Book(4, 'Computer Science', 'James Anderson', 'Absolutely worth it!', 5, 2028),
+Book(5, 'Computer Science', 'Shawn Tait', 'Must read', 1, 2027),
+Book(600, 'Swimmers', 'Michael Phelps', 'Amazing', 2, 2026)
 ]
 
-@book_app.get("/books")
+@book_app.get("/books", status_code=status.HTTP_200_OK)
 async def read_books():
     return BOOKS
 
@@ -65,8 +71,17 @@ async def create_book(book_request=Body()):
     BOOKS.append(book_request)
 '''
 
+@book_app.get('/books/publish/')
+#async def read_books_by_publish_date(published_date: int):
+async def read_books_by_publish_date(published_date: int=Query(gt=1999, lt=2031)):     # adding validation to query parameter
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
-@book_app.post('/create_newBook')
+
+@book_app.post('/create_newBook', status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):     #book request type of book request
     #print(type(book_request))
     #BOOKS.append(book_request)
@@ -90,11 +105,13 @@ def find_book_id(book_param: Book):
     return book_param
 
 
-@book_app.get('/books/{book_id}')
-async def create_book(book_id: int):
+@book_app.get('/books/{book_id}', status_code=status.HTTP_200_OK)
+#async def create_book(book_id: int):
+async def create_book(book_id: int = Path(gt=0)):     #adding validation to path parameter
     for b in BOOKS:
         if b.id == book_id:
             return b
+    raise HTTPException(status_code=404, detail="Book not found")
 
 
 '''@book_app.get('/books/{book_rating}')           
@@ -110,7 +127,8 @@ async def find_book_by_rating(book_rating: float):
 #take out specific information (e.g., here it take out only one object)
 
 @book_app.get('/books_rating')
-async def find_book_by_rating(book_rating: float):
+#async def find_book_by_rating(book_rating: float):
+async def find_book_by_rating(book_rating: float=Query(gt=0, lt=6)):  #adding validation to query parameter
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -118,16 +136,25 @@ async def find_book_by_rating(book_rating: float):
     return books_to_return
 
 
-@book_app.put('/books/update_book')
+@book_app.put('/books/update_book', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_req: BookRequest):
+    is_book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_req.book_id:
             BOOKS[i] = book_req
+            is_book_changed = True
+    if not is_book_changed:
+        raise HTTPException(status_code=404, detail="Book not found")
 
 
-@book_app.delete('/delete_books/{b_id}')
-async def delete_books(b_id: int):
+@book_app.delete('/delete_books/{b_id}', status_code=status.HTTP_204_NO_CONTENT)
+#async def delete_books(b_id: int):
+async def delete_books(b_id: int = Path(gt=0)):   #adding validation to path parameter
+    is_book_changed = False
     for b in range(len(BOOKS)):
         if BOOKS[b].id == b_id:
             BOOKS.pop(b)
+            is_book_changed = True
             break
+    if not is_book_changed:
+        raise HTTPException(status_code=404, detail="Book not found")
