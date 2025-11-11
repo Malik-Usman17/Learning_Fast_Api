15ae7b2294c91fbefbaf8f266778ec596262f885
Muhammad Usman Malik <usmaan.maalik9503@gmail.com>
2025-11-11 23:40:30 +0100

    create a user

--- Project_3_Todo_App/routers/auth.py
+++ Project_3_Todo_App/routers/auth.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from starlette import status
 from models import Users
 from passlib.context import CryptContext
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal
 
-
-'''Now API router will allow us to be able to route from our main.py file to our auth.py file.'''
 
 router = APIRouter()
+'''Now API router will allow us to be able to route from our main.py file to our auth.py file.'''
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 '''It's just some setup information and default information that we need for our pathlib to work properly.'''
@@ -19,8 +32,8 @@ class CreateUserRequest(BaseModel):
     last_name: str
     role: str
 
-@router.post("/auth/")
-async def get_user(create_user_req: CreateUserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_req: CreateUserRequest):
     create_user_model = Users(
         email = create_user_req.email,
         username = create_user_req.username,
@@ -31,5 +44,7 @@ async def get_user(create_user_req: CreateUserRequest):
         is_active = True
     )
 
+    db.add(create_user_model)
+    db.commit()
+
     return create_user_model
-    return {"message": "Welcome to the Todos API!"}
